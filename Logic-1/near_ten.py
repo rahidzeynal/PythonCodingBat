ce6dc6186510ba8a34f901c15cf950664ad21c5f
Rahid Zeynalov <44039543+rahidzeynal@users.noreply.github.com>
2022-06-22 17:21:56 +0400

    Update near_ten.py

--- Logic-1/near_ten.py
+++ Logic-1/near_ten.py
@@ -1,2 +1,8 @@
+# My 1st solution
 def near_ten(num):
   return 10 - num % 10 in range(0, 3) or num % 10 in range(0, 3)
+
+
+# My 2nd solution
+def near_ten(num):
+  return num % 10 >= 8 or num % 10 <= 2
