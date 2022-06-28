1868bec31e1a1d3bd073dde4373795ebdb53957f
Rahid Zeynalov <44039543+rahidzeynal@users.noreply.github.com>
2022-06-28 22:30:22 +0400

    Create centered_average.py

---
+++ LIst-2/centered_average.py
@@ -0,0 +1,22 @@
+def centered_average(nums):
+  l = []
+  cmn = 0
+  cmx = 0
+  mn = min(nums)
+  mx = max(nums)
+  
+  # remove dublicates
+  for i in nums:
+    # remove one of the minimums
+    if i == mn:
+      cmn += 1
+      if cmn > 1:
+        l.append(i)
+    elif i == mx:
+      cmx += 1
+      if cmx > 1:
+        l.append(i)
+    else:
+      l.append(i)
+      
+  return sum(l)/len(l)
